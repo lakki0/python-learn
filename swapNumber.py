45b9df0a6f7e17c819616625c99ebd15209b7b53
lakki <yogi.indulkar97@gmail.com>
2024-01-10 01:15:42 +0530

    files added

---
+++ swapNumber.py
@@ -0,0 +1,31 @@
+# arr = [12, 35, 9, 56, 24]
+
+# arr[0],arr[-1] = arr[-1],arr[0]
+# print(arr)
+
+
+# --------- even odd count --------
+
+# list1 = [2, 7, 5, 64, 14]
+# even_count = 0
+# odd_count = 0
+# for i in list1:
+#     if i%2==0:
+#         even_count += 1
+#     else:
+#         odd_count += 1
+        
+# print("Even : ",even_count)
+# print("odd : ",odd_count)
+
+
+list1 = [1, 2, 3]
+total = 1
+for i in list1:
+    total *= i
+    
+print(total)
+
+
+
+
