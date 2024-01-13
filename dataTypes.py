13319a48f0b0c4bb9f96732c400b9fc1af1d6d56
lakki <yogi.indulkar97@gmail.com>
2024-01-14 01:30:28 +0530

    data type study

---
+++ dataTypes.py
@@ -0,0 +1,22 @@
+# x = 10
+# y = 15.4
+# z = 2+3j
+
+# print(type(x),type(y),type(z))
+
+# x = "lakki"
+# print(type(x))
+
+# y = "24"
+# # print(type(int(y)))
+# print(type(float(y)))
+# print(type(complex(y)))
+# complex(y)
+# print(y)
+
+# print(type(True)
+
+print(type(4>5))
+print(type(5+5.7))
+
+
