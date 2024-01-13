8a38434874844fe4ee78ddd43ef0c6339e408310
lakki <yogi.indulkar97@gmail.com>
2024-01-14 02:22:44 +0530

    learn the concepts of set

--- listSetDict.py
+++ listSetDict.py
@@ -1,11 +1,24 @@
-my_list = [2,4,16,13,4,7,2]
-# print(my_list,type(my_list))
-my_list_2 = ["apple",'strawberry','banana']
+# my_set = {2,5,9,12,5,5,26,7,3,9}
+my_list = [3,5,12,5,8,6,9,5,8]
+
+newSet = set(my_list)
+print(newSet,type(newSet))
+
+# my_set.add("money")
+# print(my_set)
+
+# print(my_set[2]) # indexing are not working in  set
+
+# print(my_set)
+
+# my_list = [2,4,16,13,4,7,2]
+# # print(my_list,type(my_list))
+# my_list_2 = ["apple",'strawberry','banana']
 
 # print(my_list+my_list_2)
-my_list.extend(my_list_2)
-print(my_list)
-print(my_list_2)
+# my_list.extend(my_list_2)
+# print(my_list)
+# print(my_list_2)
 
 # del my_list[5]
 # del my_list
