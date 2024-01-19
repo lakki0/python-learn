47fd916290ca4a1465ed3d9aab17ff75d0f9e3e1
lakki <yogi.indulkar97@gmail.com>
2024-01-19 23:50:45 +0530

    understanding the numpy concepts

---
+++ numpy_.py
@@ -0,0 +1,41 @@
+
+
+# import numpy as np
+
+# arr = np.array([2,3,4,5,6,12,3,56,34])
+
+# index = np.where(arr%2==0)
+# print(arr[index])
+
+# index = np.where(arr==6)
+# print(index)
+
+# arr = np.array([[2,4,5,3],[4,7,3,7]])
+
+# arr_2 = np.sort(arr)
+# print(arr_2)
+
+# arr = np.array([2,3,4,5,6])
+# print(arr)
+
+# arr_2 = np.array([[2,4,5,3],[4,7,3,7]])
+# print(arr_2)
+
+# arr = np.array([[2,4,5,3],[4,7,3,7]])
+
+# print(arr.dtype)
+# print(arr.ndim)
+# print(arr.shape)
+
+# arr = np.array([2,3,4,5,6,12,3,56,34])
+# arr_2 = arr.reshape(3,3)
+# print(arr_2)
+
+# arr_2 = np.array([10,11,12,13])
+# arr_3 = np.concatenate((arr_2,arr))
+# print(arr_3)
+
+# arr_2 = np.array_split(arr,3)
+# arr_2 = np.array_split(arr,5)
+# print(arr_2)
+
